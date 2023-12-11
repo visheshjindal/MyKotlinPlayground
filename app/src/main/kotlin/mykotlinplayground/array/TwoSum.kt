54b151cf50651ab9770a2e539105f1f8c2ad2506
Vishesh Jindal <ece.vishesh@gmail.com>
2023-12-11 22:19:03 +0530

    Two Sum with Map

--- app/src/main/kotlin/mykotlinplayground/array/TwoSum.kt
+++ app/src/main/kotlin/mykotlinplayground/array/TwoSum.kt
@@ -1,17 +1,31 @@
 package mykotlinplayground.array
 
+import kotlin.math.abs
 
 
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
 
-fun twoSum(items: IntArray, target: Int) : IntArray {
-    for(i in items.indices) {
-        for (j in 1..<items.size) {
-            if (items[i] + items[j] == target) {
+fun twoSum(nums: IntArray, target: Int) : IntArray {
+    for(i in nums.indices) {
+        for (j in i+1..<nums.size) {
+            if (nums[i] + nums[j] == target) {
                 return intArrayOf(i, j)
             }
+        }
+    }
+    return intArrayOf()
+}
+
+fun twoSumUsingMap(nums: IntArray, target: Int) : IntArray {
+    val map = hashMapOf<Int, Int>()
+    for (i in nums.indices) {
+        val diff = target - nums[i]
+        if (map.containsKey(diff)) {
+            return intArrayOf(map.getOrDefault(diff, diff), i)
+        } else {
+            map[nums[i]] = i
         }
     }
     return intArrayOf()
--- app/src/test/kotlin/mykotlinplayground/array/ArrayTest.kt
+++ app/src/test/kotlin/mykotlinplayground/array/ArrayTest.kt
@@ -24,6 +24,19 @@ class ArrayTest {
         }
         
         @Test
+        fun `two sum using map will return a index of result`() {
+            // Arrange
+            val nums: IntArray = intArrayOf(2, 11, 5, 7)
+            val target = 9
+            // Act
+            val result = twoSumUsingMap(nums, target)
+            // Assert
+            assertTrue(message = "Index of result is wrong") {
+                result.isNotEmpty() && result[0] == 0 && result[1] == 3
+            }
+        }
+
+        @Test
         fun `two sum will return a index of result`() {
             // Arrange
             val nums: IntArray = intArrayOf(2, 11, 5, 7)
@@ -32,7 +45,46 @@ class ArrayTest {
             val result = twoSum(nums, target)
             // Assert
             assertTrue(message = "Index of result is wrong") {
-                result[0] == 0 && result[1] == 3
+                result.isNotEmpty() && result[0] == 0 && result[1] == 3
+            }
+        }
+
+        @Test
+        fun `for invalid input result should be empty array`() {
+            // Arrange
+            val nums: IntArray = intArrayOf(2, 11, 5, 1)
+            val target = 9
+            // Act
+            val result = twoSum(nums, target)
+            // Assert
+            assertTrue(message = "Array should be empty") {
+                result.isEmpty()
+            }
+        }
+
+        @Test
+        fun `for empty input list result should be empty array`() {
+            // Arrange
+            val nums: IntArray = intArrayOf()
+            val target = 9
+            // Act
+            val result = twoSum(nums, target)
+            // Assert
+            assertTrue(message = "Array should be empty") {
+                result.isEmpty()
+            }
+        }
+
+        @Test
+        fun `for duplicate elements in the list index should be different`() {
+            // Arrange
+            val nums: IntArray = intArrayOf(2, 5, 5, 1)
+            val target = 10
+            // Act
+            val result = twoSum(nums, target)
+            // Assert
+            assertTrue(message = "Index of result is wrong or both the index are same") {
+                result[0] == 1 && result[1] == 2
             }
         }
     }
